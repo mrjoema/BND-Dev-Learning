16a59493c320ecbc87112a656d44ed3b46c3b60c
Joe Ma <joe.ma@offerupnow.com>
2018-05-14 23:31:44 -0700

    upload diff repos

---
+++ albums/index.js
@@ -0,0 +1,12 @@
+// Import a library to help create component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+
+// Create a component
+const App = () => (
+    <Text>Some Text</Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
