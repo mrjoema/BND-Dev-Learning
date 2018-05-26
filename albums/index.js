fde52ef6304c30d4864c8286a87adc5e9b3873a6
Joe Ma <hokyinma@uw.edu>
2018-05-26 02:30:03 -0700

    add network call fun

--- albums/index.js
+++ albums/index.js
@@ -1,12 +1,15 @@
 // Import a library to help create component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
-
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-    <Header headerText={'My Albums'} />
+    <View>
+        <Header headerText={'My Albums'} />
+        <AlbumList />
+    </View>
 );
 
 // Render it to the device
