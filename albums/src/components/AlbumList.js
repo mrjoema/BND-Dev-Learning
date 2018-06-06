04e026bc2830ebd9cff07fb3c25335df4088bcb1
Joe Ma <joe.ma@offerupnow.com>
2018-06-06 11:42:52 -0700

    Learnt the state method to re-render component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,16 +3,24 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] };
 
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-             .then(response => console.log(response));
+             .then(response => this.setState({ albums: response.data }));
+            // setState() : only method to update the component's state
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
     }
 
     render() {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Album List !!!! </Text>
+                {this.renderAlbums()}
             </View>
         );
     }
