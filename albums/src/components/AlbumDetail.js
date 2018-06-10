dcfe966c9680f5c0a70ec0eecf9d96af86086719
Joe Ma <hokyinma@uw.edu>
2018-06-10 00:12:38 -0700

    add the button and make it more reusable

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 // functional component: We don't need lifecycle and storing any state in here
 
@@ -28,11 +29,16 @@ const AlbumDetail = ({ album }) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image 
                     style={imageStyle}
                     source={{ uri: image }}
                 />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => console.log(title)} />
             </CardSection>
         </Card>
     );
