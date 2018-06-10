a4a274a110acacd9851819a8b6a38fe7b1a7c8da
Joe Ma <hokyinma@uw.edu>
2018-06-09 23:37:05 -0700

    make the app scrollable

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -6,22 +6,33 @@ import CardSection from './CardSection';
 // functional component: We don't need lifecycle and storing any state in here
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { title, artist, thumbnail_image, image } = album;
+    const { thumbnailStyle, 
+            headerContentStyle, 
+            thumbnailContainerStyle,
+            headerTextStyle,
+            imageStyle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }} 
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                    style={imageStyle}
+                    source={{ uri: image }}
+                />
             </CardSection>
         </Card>
     );
@@ -33,9 +44,25 @@ const styles = {
         justifyContent: 'space-around'
     },
 
+    headerTextStyle: {
+        fontSize: 18
+    },
+
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
