1959dee43656622b926459240c95ce4c16caebbf
Joe Ma <hokyinma@uw.edu>
2018-06-12 23:56:51 -0700

    Draft the ui of auth app

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+    componentWillMount() {
+        // Init the firebase application
+        firebase.initializeApp({
+            apiKey: "AIzaSyDp1EV3JBkaw-wvToGi7O8MyZlYIsxvnq0",
+            authDomain: "react-native-auth-2c512.firebaseapp.com",
+            databaseURL: "https://react-native-auth-2c512.firebaseio.com",
+            projectId: "react-native-auth-2c512",
+            storageBucket: "react-native-auth-2c512.appspot.com",
+            messagingSenderId: "646167445571"
+          });
+    }
+
     render() {
         return (
             <View>
-                <Text> An app</Text>
+                <Header headerText="Aunthentication" />
+                <LoginForm />
             </View>
         )
     }
---
+++ auth/src/components/common/index.js
@@ -0,0 +1,7 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+
+// It's the easier way to make reuseable components
