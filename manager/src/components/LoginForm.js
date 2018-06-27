c948a26a771e76614d219c2c182799347aa7cbba
Joe Ma <hokyinma@uw.edu>
2018-06-27 16:51:18 -0700

    Finish the entire login fun in manager app

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 
@@ -17,6 +18,30 @@ class LoginForm extends Component {
     onButtonPress() {
         const { email, password } = this.props;
         this.props.loginUser({ email, password });
+    }
+
+    renderButton() {
+        if (this.props.loading) {
+            return <Spinner size="large"/>;
+        } else {
+            return (
+                <Button onPress={this.onButtonPress.bind(this)}>
+                    Login
+                </Button>
+            );
+        }
+    }
+
+    renderError() {
+        if (this.props.error) {
+            return (
+                <View style={{ backgroundColor: 'white' }}>
+                    <Text style={styles.errorTextStyle}>
+                        {this.props.error}
+                    </Text>
+                </View>
+            );
+        }
     }
 
     render() {
@@ -41,13 +66,21 @@ class LoginForm extends Component {
                     />
                 </CardSection>
                   
+                {this.renderError()}
+
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
@@ -55,7 +88,9 @@ const mapStateToProps = state => {
     // Step 4: Get the state by reducer and update the component state;
     return {
         email: state.auth.email,
-        password: state.auth.password
+        password: state.auth.password,
+        error: state.auth.error,
+        loading: state.auth.loading
     };
 };
 
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,13 +1,17 @@
 import { 
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
-    LOGIN_USER_SUCCESS
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER_LOADING
  } from '../actions/types';
 
 const INITIAL_STATE = { 
     email: '', 
     password: '',
-    user: null
+    user: null,
+    error: '',
+    loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -19,7 +23,11 @@ export default (state = INITIAL_STATE, action) => {
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload }
+            return { ...state, ...INITIAL_STATE, user: action.payload};
+        case LOGIN_USER_FAIL:
+            return {...state, error: 'Authentication Failed.', loading: false }
+        case LOGIN_USER_LOADING:
+            return {...state, loading: true, error: ''}
         default:
             return state;
     }
