cbac4679fe503341597d2165a91cc5c3211eb16b
Joe Ma <hokyinma@uw.edu>
2018-06-27 01:35:14 -0700

    Polish the login function in manager app

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
@@ -21,8 +22,9 @@ class App extends Component {
     }
 
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
-            <Provider store={createStore(reducers)}>
+            <Provider store={store}>
                 <View style = {{marginTop: 20}}>
                     <LoginForm />
                 </View>
--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
@@ -12,6 +12,11 @@ class LoginForm extends Component {
 
     onPasswordChange(text) {
         this.props.passwordChanged(text);
+    }
+
+    onButtonPress() {
+        const { email, password } = this.props;
+        this.props.loginUser({ email, password });
     }
 
     render() {
@@ -37,7 +42,7 @@ class LoginForm extends Component {
                 </CardSection>
                   
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Login
                     </Button>
                 </CardSection>
@@ -56,4 +61,4 @@ const mapStateToProps = state => {
 
 // Step 2: emailChanged: the new action which is gonna be sent to reducer
 // Step 3: mapStateToProps: the new state emitted by the reducer
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
